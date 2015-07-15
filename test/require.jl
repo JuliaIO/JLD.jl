0d984a2a4ab81fed5a57aee8fb4f998505c9726e
Tim Holy <tim.holy@gmail.com>
2015-07-15 05:45:15 -0700

    Split JLD into its own package

--- test/require.jl
+++ test/require.jl
@@ -7,7 +7,7 @@ include("JLDTest.jl")
 function create()
     x = JLDTest(convert(Int16, 5))  # int16 makes this work on 0.2
     jldopen("require.jld", "w") do file
-        addrequire(file, joinpath(Pkg.dir(), "HDF5", "test", "JLDTest.jl"))
+        addrequire(file, joinpath(Pkg.dir(), "JLD", "test", "JLDTest.jl"))
         truncate_module_path(file, JLDTemp)
         write(file, "x", x)
     end
