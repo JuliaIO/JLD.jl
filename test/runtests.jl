fff2c3fac625650d9db86ab0a980cce237eb6856
Rene Donner <code@donner.at>
2015-08-03 15:21:28 +0200

    rename jld.jl to avoid test hang on OSX

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using HDF5
 
 runtest(filename) = (println(filename); include(filename))
 
-runtest("jld.jl")
+runtest("jldtests.jl")
 runtest("require.jl")
 if Pkg.installed("DataFrames") != nothing
     runtest("jld_dataframe.jl")
