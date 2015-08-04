d338f74a350f989ddaf796c21dd7a5ff303c6c60
Tim Holy <tim.holy@gmail.com>
2015-08-04 08:29:40 -0500

    A better require test

--- test/require.jl
+++ test/require.jl
@@ -2,10 +2,10 @@ using HDF5, JLD
 
 module JLDTemp
 using HDF5, JLD
-include("JLDTest.jl")
+include("JLDTest.jl")  # Private module inside JLDTemp
 
 function create()
-    x = JLDTest(convert(Int16, 5))  # int16 makes this work on 0.2
+    x = JLDTest.Object(convert(Int16, 5))  # int16 makes this work on 0.2
     jldopen("require.jld", "w") do file
         addrequire(file, :JLDTest)
         truncate_module_path(file, JLDTemp)
@@ -20,7 +20,7 @@ push!(LOAD_PATH, splitdir(@__FILE__)[1])
 x = jldopen("require.jld") do file
     read(file, "x")
 end
-@assert typeof(x) == JLDTest
+@assert typeof(x) == JLDTest.Object
 @assert x.data == 5
 pop!(LOAD_PATH)
 rm("require.jld")
