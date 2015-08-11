033ad195794b2cd5e42cc90229ce85303bdbe82c
Tim Holy <tim.holy@gmail.com>
2015-08-10 21:22:30 -0500

    Turn on automatic precompilation

--- src/JLD.jl
+++ src/JLD.jl
@@ -2,6 +2,8 @@
 ## Reading and writing Julia data .jld files ##
 ###############################################
 
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
+
 module JLD
 using HDF5, Compat
 # Add methods to...
