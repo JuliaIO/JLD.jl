43611243a4833efccb509a214e447154f1a5b8d5
Dmytro S Lituiev <d.lituiev@gmail.com>
2015-08-29 19:08:41 -0700

    read and load functionality for JLD files containing groups

--- src/JLD.jl
+++ src/JLD.jl
@@ -311,6 +311,15 @@ readmmap(obj::JldDataset, args...) = readmmap(obj.plain, args...)
 end
 @compat read(parent::Union{JldFile,JldGroup}, name::Symbol) = read(parent,bytestring(string(name)))
 
+function read(obj::JldGroup)
+    nms = names(obj)
+    val = Dict{ByteString, Any}()
+    for nm in nms
+        val[nm] = read(obj[nm])
+    end
+    return val
+end
+
 function read(obj::JldDataset)
     dtype = datatype(obj.plain)
     dspace_id = HDF5.h5d_get_space(obj.plain)
